e8ed4f9dec358966f91dd905d2570d8f2e9b5916
hungit1993qt <hungit1993qt@gmail.com>
2022-10-13 01:07:15 +0700

    fix position mouse

--- js/cursorsBox.js
+++ js/cursorsBox.js
@@ -1,19 +1,32 @@
 var cursorTag = document.querySelector("div.wraper");
 var box = cursorTag.querySelector("div.cursorsBox");
+var editWidth = document.querySelector("section");
+var nav = document.querySelector("div.navbar");
+var aside = document.querySelector("div.navbar");
+var a = aside.offsetWidth;
+
+function asideToggle() {
+  const iconBtn = document.querySelector("div.navbar button.show img");
+  iconBtn.classList.toggle("rotate");
+  aside.classList.toggle("show");
+
+  if (aside.classList.contains("show")) {
+    a = 0;
+  } else {
+    a = aside.offsetWidth;
+  }
+}
 
 function boxMove(e) {
   box.style.display = "block";
   box.style.opacity = "1";
   box.style.left = e.pageX + "px";
   box.style.top = e.pageY + "px";
+  const x = ((e.pageX - a) * 100) / (cursorTag.offsetWidth - a) + "%";
+  const y = (e.pageY * 100) / window.innerHeight + "%";
+  box.style.transform = `translate(-${x},-${y}`;
 }
 function clearBoxMove() {
   box.style.display = "none";
   box.style.opacity = "0";
-}
-function asideToggle() {
-  const aside = document.querySelector("div.navbar");
-  const iconBtn = document.querySelector("div.navbar button.show img");
-  iconBtn.classList.toggle("rotate");
-  aside.classList.toggle("show");
 }
