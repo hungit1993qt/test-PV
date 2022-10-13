25ad7c6aedf6b94365cc7d8fd1ad68be3ca47f2b
hungit1993qt <hungit1993qt@gmail.com>
2022-10-13 09:34:55 +0700

    fix

--- js/cursorsBox.js
+++ js/cursorsBox.js
@@ -22,9 +22,21 @@ function boxMove(e) {
   box.style.opacity = "1";
   box.style.left = e.pageX + "px";
   box.style.top = e.pageY + "px";
-  const x = ((e.pageX - a) * 100) / (cursorTag.offsetWidth - a) + "%";
-  const y = (e.pageY * 100) / window.innerHeight + "%";
-  box.style.transform = `translate(-${x},-${y}`;
+  const x = ((e.pageX - a) * 100) / (cursorTag.offsetWidth - a);
+  const y = (e.pageY * 100) / window.innerHeight;
+  console.log(x, y);
+  if (x < 50 && y < 50) {
+    box.style.transform = `translate(0,0)`;
+  }
+  if (x < 50 && y > 50) {
+    box.style.transform = `translate(0,-100%)`;
+  }
+  if (x > 50 && y > 50) {
+    box.style.transform = `translate(-100%,-100%)`;
+  }
+  if (x > 50 && y < 50) {
+    box.style.transform = `translate(-100%,0)`;
+  }
 }
 function clearBoxMove() {
   box.style.display = "none";
